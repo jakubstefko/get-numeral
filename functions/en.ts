62da55b9e8821fa892f934a63e416a140f03f393
JakubStefko <jakub.stefko@gmail.com>
2023-06-23 13:19:57 +0200

    polish language improvements and language function polishing

--- functions/en.ts
+++ functions/en.ts
@@ -1,10 +1,10 @@
 export const en = (n: number, isDashAdded: boolean) => {
   const lastDigit = n.toString().slice(-1);
-  const middleChar = isDashAdded ? "-" : ""
+  const core = `${n}${isDashAdded ? "-" : ""}`
 
-  if (lastDigit === "1") return `${n}${middleChar}st`;
-  if (lastDigit === "2") return `${n}${middleChar}nd`;
-  if (lastDigit === "3") return `${n}${middleChar}rd`;
-  if (n % 10 === 0) return `${n}${middleChar}ieth`;
-  return `${n}${middleChar}th`;
+  if (lastDigit === "1") return `${core}st`;
+  if (lastDigit === "2") return `${core}nd`;
+  if (lastDigit === "3") return `${core}rd`;
+  if (n % 10 === 0) return `${core}ieth`;
+  return `${core}th`;
 };
--- functions/pl.ts
+++ functions/pl.ts
@@ -1,10 +1,11 @@
 export const pl = (n: number, isDashAdded: boolean) => {
   const lastDigit = n.toString().slice(-1);
-  const middleChar = isDashAdded ? "-" : ""
+  const core = `${n}${isDashAdded ? "-" : ""}`
 
-  if (lastDigit === "1") return `${n}${middleChar}szy`;
-  if (lastDigit === "2") return `${n}${middleChar}gi`;
-  if (lastDigit === "3") return `${n}${middleChar}ci`;
-  if (lastDigit === "7" || lastDigit === "8") return `${n}${middleChar}my`;
-  return `${n}${middleChar}ty`;
+  if (n > 8 && n < 21) return `${core}ty`
+  if (lastDigit === "1") return `${core}szy`;
+  if (lastDigit === "2") return `${core}gi`;
+  if (lastDigit === "3") return `${core}ci`;
+  if (lastDigit === "7" || lastDigit === "8") return `${core}my`;
+  return `${core}ty`;
 };
